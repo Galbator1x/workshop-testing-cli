01b95e559ad1a785893a0be6ac8c4017e6d36e45
Ivan Filimonov <snake73w@gmail.com>
2018-10-13 18:27:11 +0700

    fix

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -4,7 +4,7 @@ module Weather
   class CLI < Thor
     desc 'weather CITY', 'returns weather in CITY'
     def weather(city)
-      puts Weather::Weather.get_weather_in_city(city)
+      puts Weather.get_weather_in_city(city)
     end
   end
 end
